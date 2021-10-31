082f05c91d180e990f992fc410ec1d3b244ca6ec
Ahmed Jama <ahmedmusejama1997@gmail.com>
2021-10-31 12:31:37 +0000

    Main: Able to detect if the file is video or audio. Error handling if file cannot be found.

--- bin/main.py
+++ bin/main.py
@@ -1,11 +1,16 @@
+import mimetypes
 import time
 import wave, math, contextlib
 import speech_recognition as sr
 from moviepy.editor import AudioFileClip
 from pathlib import Path
+import magic
+import os.path
+from os import path
+
 
 def TranscribeVideo (filepath):
-    transcribed_audio_file_name = "transcribed_speech.wav"
+    transcribed_audio_file_name = "transcribed_video_speech.wav"
     video_file_name = filepath
     audioclip = AudioFileClip(video_file_name)
     audioclip.write_audiofile(transcribed_audio_file_name)
@@ -20,7 +25,7 @@ def TranscribeVideo (filepath):
             audio = r.record(source, offset=z * 60, duration=60)
         timestr = time.strftime("%Y%m%d")
         filename = Path(filepath).name
-        video_transcript_folderpath = './transcripts/Video/'
+        video_transcript_folderpath = '../transcripts/Video/'
         f = open(video_transcript_folderpath + timestr + "-" + str(filename) + "-" + "transcription.txt", "a")
         speech = r.recognize_google(audio, language='en-GB', show_all=True)
         print(speech)
@@ -29,7 +34,7 @@ def TranscribeVideo (filepath):
     f.close()
 
 def TranscribeAudio (filepath):
-    transcribed_audio_file_name = "transcribed_speech.wav"
+    transcribed_audio_file_name = "transcribed_audio_speech.wav"
     audioclip = AudioFileClip(filepath)
     audioclip.write_audiofile(transcribed_audio_file_name)
 
@@ -42,27 +47,29 @@ def TranscribeAudio (filepath):
     for z in range(0, total_duration):
         with sr.AudioFile(transcribed_audio_file_name) as source:
             audio = r.record(source, offset=z * 60, duration=60)
-        timestr = time.strftime("%Y%m%d-%H%M%S")
+        timestr = time.strftime("%Y%m%d")
         filename = Path(filepath).name
-        Audio_transcript_folderpath = "./transcripts/Audio/"
+        Audio_transcript_folderpath = "../transcripts/Audio/"
         f = open(Audio_transcript_folderpath + timestr + "-" + str(filename) + "-" + "transcription.txt", "a")
-        speech = r.recognize_sphinx(audio, language='en', show_all=True)
+        speech = r.recognize_google(audio, language='en', show_all=True)
         print(speech)
         f.write(str(speech))
         f.write(" ")
     f.close
 
 if __name__ == '__main__':
-    filepath = input("Please input the path to your file:    ")
-    if filepath.endswith('.mp4') or filepath.endswith('.mp3'):
-        print(filepath)
+    mime = magic.Magic(mime=True)
+    filepath = input("Please input the path to your file:")
+    if path.exists(filepath):
+        FileType = mime.from_file(filepath)
+        print(FileType)
+        if str(FileType).__contains__("audio"):
+            TranscribeAudio(filepath)
+        else:
+            if str(FileType).__contains__("video"):
+                TranscribeVideo(filepath)
+            else:
+                print("Not Valid File")
     else:
-        print("empty/invalid input")
-        filepath = input("Please input the path to your file")
-    source_type = input("Is this a Video or Audio   ")
-    if source_type == "Video":
-        TranscribeVideo(filepath)
-    else:
-        if source_type == "Audio":
-            print("Audio is not supported yet")
-            exit(0)
+        print("File does not exist")
+        exit(0)
